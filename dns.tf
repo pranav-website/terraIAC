1cf6ec8d79e3747f786fb92bade992818decf9dd
FloatRun <pranavdumpa@gmail.com>
2026-04-08 10:51:26 -0700

    cloudflare to vps forwarding certs

--- dns.tf
+++ dns.tf
@@ -4,8 +4,15 @@ variable "CLOUDFLARE_GLOBAL_API_TOKEN" {
   sensitive   = true
 }
 
+# manually entered, variable
 variable "CLOUDFLARE_EMAIL" {
   description = "Cloudflare Global API email"
+  type        = string
+  sensitive   = true
+}
+
+variable "CLOUDFLARE_ACCOUNT_ID" {
+  description = "Cloudflare Account ID"
   type        = string
   sensitive   = true
 }
@@ -13,6 +20,21 @@ variable "CLOUDFLARE_EMAIL" {
 provider "cloudflare" {
   email   = var.CLOUDFLARE_EMAIL
   api_key = var.CLOUDFLARE_GLOBAL_API_TOKEN
+}
+
+# # dynamically grabbed, data
+# resource "cloudflare_zone" "main_zone" {
+#   account = {
+#     id = "var.CLOUDFLARE_ACCOUNT_ID"
+#   }
+#   name = "pranavdumpa.win"
+#   type = "full"
+# }
+
+variable "cloudflare_zone_id" {
+  type = string
+  description = "Cloudflare Zone ID for pranavdumpa.win, can be found in Cloudflare dashboard"
+  sensitive = true
 }
 
 resource "tls_private_key" "origin" {
@@ -37,6 +59,24 @@ resource "cloudflare_origin_ca_certificate" "origin" {
   csr          = tls_cert_request.origin.cert_request_pem
   hostnames    = ["pranavdumpa.win", "www.pranavdumpa.win"]
   request_type = "origin-rsa"
+}
+
+resource "cloudflare_dns_record" "cf_to_ec2_A_record" {
+  zone_id = var.cloudflare_zone_id
+  name    = "@"
+  content = aws_instance.web.public_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
+
+resource "cloudflare_dns_record" "www_cf_to_main_cname_record" {
+  zone_id = var.cloudflare_zone_id
+  name    = "www"
+  content = "pranavdumpa.win"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
 }
 
 output "origin_cert_pem" {
